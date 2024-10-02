f513d1d068aac9f72777e7e611953f28c9bd0961
David S D <sddavid.johan@gmail.com>
2024-10-02 14:27:40 +0530

    feat: smoothly rounded qr

--- src/app/tools/qr-generator/page.tsx
+++ src/app/tools/qr-generator/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { Logo } from "@/assets/icons/Logo";
 import { Section } from "@/components/common/Section";
-import { QrView } from "react-dynamic-qr";
+import { QrView } from "@/components/tools/QrView";
 
 const Page = () => {
   return (
     <Section>
       <div>
-        <QrView size={300} content="https://davidsling.in" />
+        <QrView size={300} content="https://davidsling.in">
+          <div className="bg-dark flex items-center justify-center h-full rounded-full">
+            <Logo className="h-5 w-5 ml-1 fill-primary" />
+          </div>
+        </QrView>
       </div>
     </Section>
   );
--- src/components/tools/QrView.tsx
+++ src/components/tools/QrView.tsx
@@ -1,8 +1,77 @@
 import { cn } from "@/utils/cn";
 import QrCode from "@/utils/qr";
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-export const QrView: FC<{ content: string }> = ({ content }) => {
+const getAdjacents = (
+  matrix: number[][],
+  x: number,
+  y: number,
+  options?: { min?: number; max?: number }
+) => {
+  const coords = {
+    top: { x, y: y - 1 },
+    right: { x: x + 1, y },
+    bottom: { x, y: y + 1 },
+    left: { x: x - 1, y },
+  };
+  const boundsExist = options?.min && options.max;
+
+  const isTopWithinBounds = boundsExist
+    ? coords.top.y < options.min! ||
+      coords.top.y > options.max! ||
+      coords.top.x < options.min! ||
+      coords.top.x > options.max!
+    : true;
+  const top = isTopWithinBounds ? matrix[coords.top.y]?.[coords.top.x] : 0;
+
+  const isRightWithinBounds = boundsExist
+    ? coords.right.y < options.min! ||
+      coords.right.y > options.max! ||
+      coords.right.x < options.min! ||
+      coords.right.x > options.max!
+    : true;
+  const right = isRightWithinBounds
+    ? matrix[coords.right.y]?.[coords.right.x]
+    : 0;
+
+  const isBottomWithinBounds = boundsExist
+    ? coords.bottom.y < options.min! ||
+      coords.bottom.y > options.max! ||
+      coords.bottom.x < options.min! ||
+      coords.bottom.x > options.max!
+    : true;
+  const bottom = isBottomWithinBounds
+    ? matrix[coords.bottom.y]?.[coords.bottom.x]
+    : 0;
+
+  const isLeftWithinBounds = boundsExist
+    ? coords.left.y < options.min! ||
+      coords.left.y > options.max! ||
+      coords.left.x < options.min! ||
+      coords.left.x > options.max!
+    : true;
+  const left = isLeftWithinBounds ? matrix[coords.left.y]?.[coords.left.x] : 0;
+  return { top, right, bottom, left };
+};
+
+const getRoundedCorners = ({
+  bottom,
+  left,
+  right,
+  top,
+}: ReturnType<typeof getAdjacents>) => {
+  const bottomLeft = !bottom && !left;
+  const bottomRight = !bottom && !right;
+  const topLeft = !top && !left;
+  const topRight = !top && !right;
+  return { bottomLeft, bottomRight, topLeft, topRight };
+};
+
+export const QrView: FC<{
+  content: string;
+  size?: number;
+  children?: ReactNode;
+}> = ({ content, size = 500, children }) => {
   const matrix = (QrCode as any).generate(content, { minVersion: 3 })
     .buffer as number[];
   console.log(matrix);
@@ -14,27 +83,54 @@ export const QrView: FC<{ content: string }> = ({ content }) => {
     return acc;
   }, [] as number[][]);
   const center = Math.floor(matrixSize / 2);
-  const clearance = 4;
+  const clearance = children ? 4 : 0;
   const min = center - clearance;
   const max = center + clearance;
 
+  const logoSize = Math.floor(size / 5);
+
   return (
-    <div className="bg-white p-5 h-[500px] w-[500px] flex flex-col">
+    <div
+      style={{ height: size, width: size }}
+      className="bg-white p-5 flex flex-col relative rounded-xl"
+    >
       {matrix2.map((row, yidx) => (
         <div key={yidx} className={cn("flex flex-1")}>
-          {row.map((cell, xidx) => (
-            <div
-              key={xidx}
-              className={cn(
-                "flex-1 rounded-full",
-                cell && (xidx < min || xidx > max || yidx < min || yidx > max)
-                  ? "bg-black"
-                  : "bg-white"
-              )}
-            />
-          ))}
+          {row.map((cell, xidx) => {
+            const adjacents = getAdjacents(matrix2, xidx, yidx, { min, max });
+            const radii = getRoundedCorners(adjacents);
+            return (
+              <div
+                onClick={() => {
+                  console.log(adjacents);
+                }}
+                style={{
+                  borderRadius: `${radii.topLeft ? "50%" : "0"} ${
+                    radii.topRight ? "50%" : "0"
+                  } ${radii.bottomRight ? "50%" : "0"} ${
+                    radii.bottomLeft ? "50%" : "0"
+                  }`,
+                }}
+                key={xidx}
+                className={cn(
+                  "flex-1",
+                  cell && (xidx < min || xidx > max || yidx < min || yidx > max)
+                    ? "bg-black"
+                    : "bg-white"
+                )}
+              />
+            );
+          })}
         </div>
       ))}
+      {!!children && (
+        <div
+          style={{ height: logoSize, width: logoSize }}
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
