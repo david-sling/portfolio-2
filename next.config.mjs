97424700485d4d463ff8bedf90b92937f46fbdd4
David S D <me@davidsling.in>
2025-03-05 21:09:41 +0530

    feat: resume redirect

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/resume.pdf",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
