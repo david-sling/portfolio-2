0c038ff521858db42a2c6b1b2d858174fdadb0d1
David S D <david.sd@intelxlabs.com>
2025-07-23 16:42:09 +0530

    feat: filmkit anim

--- src/assets/illustrations/filmkit/FilmkitIllustration.tsx
+++ src/assets/illustrations/filmkit/FilmkitIllustration.tsx
@@ -3,7 +3,7 @@ import { FilmkitLogo } from "@/assets/icons/FilmkitLogo";
 export const FilmkitIllustration = () => (
   <div className="flex-1 bg-[#FFC953] flex items-center">
     <div className="flex flex-1 items-center">
-      <FilmkitLogo className="h-20 w-20 md:h-40 md:w-40 ml-5 md:ml-10 -mr-10 md:-mr-20 z-10" />
+      <FilmkitLogo className="h-20 w-20 md:h-40 md:w-40 ml-20 md:ml-40 group-hover:ml-5 md:group-hover:ml-10 -mr-10 md:-mr-20 z-10 transition-all duration-1000" />
       <div className="bg-dark flex-1 h-[77px] md:h-[154px]" />
     </div>
   </div>
