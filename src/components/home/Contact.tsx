8e4b5eae64d6b65e34e602d6b37cfb8acebbafcf
David S D <me@davidsling.in>
2026-05-10 15:27:08 +0530

    Update Contact.tsx

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,7 +1,6 @@
-import { PhoneIcon } from "@/assets/icons/PhoneIcon";
-import { Section } from "../common/Section";
 import { MailIcon } from "@/assets/icons/MailIcon";
 import Link from "next/link";
+import { Section } from "../common/Section";
 
 export const Contact = () => (
   <Section id="contact" className="py-10 pb-32 md:py-32 bg-dark-800">
@@ -9,12 +8,6 @@ export const Contact = () => (
       <span className="text-primary">this.</span>contact
     </h2>
     <div className="mt-10 space-y-2">
-      <Link href="tel:+918072539328">
-        <div className="flex items-center space-x-2">
-          <PhoneIcon />
-          <p className="text-base md:text-lg font-semibold">+91 80725 39328</p>
-        </div>
-      </Link>
       <Link href="mailto:me@davidsling.in">
         <div className="flex items-center space-x-2">
           <MailIcon />
